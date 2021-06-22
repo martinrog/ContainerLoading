a9d466fa370931afb200bb2039913e4e56f8df56
martinrog <martinrog14@gmail.com>
2021-06-23 00:01:29 +0200

    New function to see if all the boxes fit in one container.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from Box import Box
 from Container import Container
 from ContainerApi import *
+from Costs import Costs
 
 fitnessbankjeQ = int(input("Hoeveel dozen met fitnessbankjes wilt u vervoeren: "))
 fitnessmatQ = int(input("Hoeveel dozen met fitnessmatten wilt u vervoeren: "))
@@ -14,4 +15,15 @@ fitnessMat3 = Box(0.63,0.2,0.2,"Purple/Pink")
 fitnessBankje = Box(1.23,0.32,0.18,"Black/Red")
 flessen = Box(0.77,0.395,0.295,"Black")
 voetballen = Box(0.6,0.4,0.4,"default")
+
+def fitCheck():
+    totalVolumeMat1 = fitnessMat1.get_volume()*fitnessmatQ
+    totalVolumeBankje = fitnessBankje.get_volume()*fitnessbankjeQ
+    totalVolumeFlessen = flessen.get_volume()*flessenQ
+    totalVolumeVoetballen = voetballen.get_volume()*voetballenQ
+
+    if (totalVolumeBankje+totalVolumeFlessen+totalVolumeVoetballen+totalVolumeMat1) > container.get_volumeC():
+        quit(print("\nNOTE: The given quantities do not fit in one container, try again.\n\n"))
+
+fitCheck()
 
