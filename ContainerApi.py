7f685626ae58f461e073749d5073372eed8825e3
martinrog <martinrog14@gmail.com>
2021-06-23 17:32:48 +0200

    colors of the boxes changed

--- ContainerApi.py
+++ ContainerApi.py
@@ -23,8 +23,8 @@ data = {
     "username": "martin.rog",
     "api_key": "5633da16b9f7c6337cc4be333ab05cff",
     "params": {"images_background_color": "192,192,192", "images_bin_border_color": "59,59,59",
-               "images_bin_fill_color": "230,230,230", "images_item_border_color": "214,79,79",
-               "images_item_fill_color": "177,14,14", "images_item_back_border_color": "215,103,103",
+               "images_bin_fill_color": "230,230,230", "images_item_border_color": "0,0,0",
+               "images_item_fill_color": "14,177,96", "images_item_back_border_color": "0,0,0",
                "images_sbs_last_item_fill_color": "99,93,93", "images_sbs_last_item_border_color": "145,133,133",
                "images_width": 250, "images_height": 250, "images_source": "file", "images_sbs": 1,"images_complete": 1}}
 
